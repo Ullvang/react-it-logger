17f354e0d7f204c14a209a1c847227d3057ee37f
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-02-01 18:48:07 +0100

    #81

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
 import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   useEffect(() => {
@@ -16,7 +18,7 @@ function App() {
     M.AutoInit();
   }, []);
   return (
-    <>
+    <Provider store={store}>
       <SearchBar />
       <div className="container">
         <AddBtn />
@@ -26,7 +28,7 @@ function App() {
         <TechListModal />
         <Logs />
       </div>
-    </>
+    </Provider>
   );
 }
 
